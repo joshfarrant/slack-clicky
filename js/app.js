d899e1714722c3b39169fc867492dad23545dd0f
joshfarrant <joshuafefarrant@gmail.com>
2015-04-12 14:38:48 +0100

    Removed unused console.log

--- js/app.js
+++ js/app.js
@@ -273,8 +273,6 @@ function setBadgeSuccess(id, ts) {
   var badge = $('#' + id);
   var name = badge.text();
 
-  console.log(id, ts);
-  console.log(badge);
   badge.width(badge.width() + 1); // Fixes badge width to it's current width
 
   badge.addClass('share-success').removeClass('share-error');
@@ -766,9 +764,6 @@ $(document).ready(function() {
         "total_users": total_users
       }
     };
-
-
-    console.log(window.intercomSettings);
 
     // Intercom analytics
     (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/conjr0rg';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
