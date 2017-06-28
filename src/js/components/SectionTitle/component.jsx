08f13504554686420267292bee5e8fb92f829177
Josh Farrant <josh@farrant.me>
2017-06-28 16:46:52 +0100

    Added optional info icon to SectionTitle

--- src/js/components/SectionTitle/component.jsx
+++ src/js/components/SectionTitle/component.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CSSModules from 'react-css-modules';
+import ReactTooltip from 'react-tooltip';
+import InfoIcon from '../icons/InfoIcon';
 import styles from './style.scss';
 
-const SectionTitle = ({ title }) => (
-  <div styleName="section-title">{title}</div>
+const SectionTitle = ({ info, title }) => (
+  <div styleName="container">
+    <div styleName="section-title">{title}</div>
+    {info && (
+      <div styleName="icon-container">
+        <InfoIcon
+          styleName="info-icon"
+          data-tip={info}
+        />
+        <ReactTooltip
+          delayShow={300}
+          effect="solid"
+          place="bottom"
+          type="dark"
+        />
+      </div>
+    )}
+  </div>
 );
 
 SectionTitle.propTypes = {
+  info: PropTypes.string,
   title: PropTypes.string.isRequired,
+};
+
+SectionTitle.defaultProps = {
+  info: '',
 };
 
 export default CSSModules(SectionTitle, styles);
