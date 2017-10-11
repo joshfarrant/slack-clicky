f92beaa29a025846720e6f22625a04ee63b3e8b6
Josh Farrant <josh@farrant.me>
2017-10-11 21:19:29 +0100

    Minor tweaks

--- src/js/options.js
+++ src/js/options.js
@@ -1,7 +1,7 @@
 document.getElementById('clear').addEventListener('click', () => {
   chrome.runtime.sendMessage({ type: 'CLEAR_STATE' }, (response) => {
     if (response.ok === true) {
-      alert('Date cleared!'); // eslint-disable-line
+      alert('Data cleared!'); // eslint-disable-line
     }
   });
 }, false);
